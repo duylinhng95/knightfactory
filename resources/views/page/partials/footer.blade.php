8bb59cbf56278c2bf5c369853d28c559510611df
phan anh khoa <anhkhoaphan113@gmail.com>
2018-01-15 10:48:40 +0700

    master_index

---
+++ resources/views/page/partials/footer.blade.php
@@ -0,0 +1,40 @@
+<!-- Footer#6
+============================================= -->
+<footer id="footer6" class="footer footer-6 text-center bg-overlay bg-overlay-dark">
+    <div class="bg-section">
+        <img src="assets/images/background/bg-6.jpg" alt="Background"/>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <h3>Stay In Touch</h3>
+                <div class="footer-subs sub-1">
+                    <p>Subscribe for latest news & updates and promotions<br>don’t worry we’ll not spam you.</p>
+                    <div class="sub-form center-block">
+                    <form id="mailchimp" class="form-inline">
+                        <input type="email" id="email" class="form-control" name="email" placeholder="Your Email Address*" required/>
+                        <button class="btn btn-primary" type="submit">Subscribe</button>
+                    </form>
+                </div>
+                </div>
+                <div class="widget-social">
+                    <a class="facebook" href="#">
+                                <i class="fa fa-facebook"></i>
+                    </a>
+                    <a class="twitter" href="#" >
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="dribbble" href="#">
+                        <i class="fa fa-dribbble"></i>
+                    </a>
+                    <a class="vimeo" href="#">
+                        <i class="fa fa-vimeo"></i>
+                    </a>
+                </div>
+                <div class="widget-copyright">
+                    <p>&copy; Zytheme 2016, All Rights Reserved.</p>
+                </div>
+            </div><!-- .col-md-12 end -->
+        </div><!-- .row end -->
+    </div><!-- .container end -->
+</footer><!-- #footer6 end -->
---
+++ resources/views/page/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header id="navbar-spy" class="header-onepage">
+    <nav id="primary-menu" class="navbar navbar-fixed-top style-1 affix-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+                <a class="logo" href="index.html">
+                    <img src="assets/images/logo/logo.png" alt="Interact">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-left">
+                    <li class="active">
+                        <a data-scroll="scrollTo" href="#hero">home</a>
+                    </li>
+                    <li>
+                        <a data-scroll="scrollTo" href="#featured5">why us</a>
+                    </li>
+                    <li>
+                        <a data-scroll="scrollTo" href="#portfolio2">portfolio</a>
+                    </li>
+                    <li>
+                        <a data-scroll="scrollTo" href="#testimonials4">testimonials</a>
+                    </li>
+                    <li>
+                        <a data-scroll="scrollTo" href="#team3">our team</a>
+                    </li>
+                    <li>
+                        <a data-scroll="scrollTo" href="#footer6">get in touch</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+</header>
